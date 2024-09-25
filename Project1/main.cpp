11b7def42d7c2aacf470e09c587b0e5692badcbd
Dasadno <Yakaleks96@gmail.com>
2024-09-25 15:25:37 +0500

    dasdas

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -5,6 +5,9 @@ int main() {
 
 	float num;
 	float sum = 0;
+	int evensum;
+	float oddsum;
+
 
 	std::cout << "В этой программе вы вводите числа и складываете их, пока не введете 0\n\n\n\n ";
 	do
@@ -12,7 +15,14 @@ int main() {
 		std::cout << "Введите число: ";
 		std::cin >> num;
 		sum += num;
-
+		if (num / 2)
+		{
+			evensum += num;
+		}
+		else
+		{
+			oddsum += num;
+		}
 		
 	} while (num != 0);
 	
