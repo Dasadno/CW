363ac9968b74dac294b8589ecb1c07b89fcc57dc
Dasadno <Yakaleks96@gmail.com>
2024-09-25 15:21:13 +0500

    dasdsa

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,19 +3,20 @@
 int main() {
 	setlocale(LC_ALL, "ru");
 
-	float sum;
-	int answer;
+	float num;
+	float sum = 0;
 
-	std::cout << "В этой программе вы вводите числа пока "
-	while (true)
+	std::cout << "В этой программе вы вводите числа и складываете их, пока не введете 0\n\n\n\n ";
+	do
 	{
-		std::cout << "Введите число: "
-
+		std::cout << "Введите число: ";
+		std::cin >> num;
+		sum += num;
 
 		
-	}
-
-
+	} while (num != 0);
+	
+	std::cout << "Сумма всех чисел =" << sum;
 
 
 
