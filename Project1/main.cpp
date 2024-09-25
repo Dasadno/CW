e6502b785e5c9519c4fbb4cce8e235227293dd41
Dasadno <Yakaleks96@gmail.com>
2024-09-25 15:11:59 +0500

    Добавьте файлы проекта.

---
+++ Project1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	float sum;
+	int answer;
+
+	std::cout << "В этой программе вы вводите числа пока "
+	while (true)
+	{
+		std::cout << "Введите число: "
+
+
+		
+	}
+
+
+
+
+
+	return 0;
+}
