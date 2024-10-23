fc5618d538aae5ad74d1714f2cc7558f9704de66
Dasadno <Yakaleks96@gmail.com>
2024-10-23 16:41:35 +0500

    dasdsa

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,36 +3,45 @@
 #include <string.h>
 using namespace std;
 
-
-template <typename arb>
-arb avg_arr(arb arr1[], int size)
+bool registration(string login, string password)
 {
-
-	double sum1 = 0;
-
-
-	for (int i = 0; i < size; i++)
+	if (end(login) - begin(login) > 15 || end(password) - begin(password) > 15)
 	{
-		cout << arr1[i] << " ";
-		sum1 += arr1[i];
+		
+		return false;
 	}
-	cout << endl;
-
-
-	return sum1 / size;
-
+	else
+	{
+		return true;
+	}
 }
 
-int main() {
+int main(){
+
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	const int array_size = 5;
-	float arr1[array_size]{ 1.3, 5.3, 2.5, 4.2, 7.6 };
-	int arr2[array_size]{ 1,2,3,4,5 };
-	double arr3[array_size]{ 5.321, 8.13551, 4.3232, 52.312, 61.23125 };
 
-	cout << avg_arr(arr3, array_size);
+	string login;
+	string password;
+
+	cout << "Введите логин: ";
+	cin >> login;
+	cout << "Введите пароль: ";
+	cin >> password;
+
+	bool reg = registration(login, password);
+	if (reg == true)
+	{
+		cout << "Регистрация прошла успешно \n";
+		cout << "Ваш логин: " << login << "\n";
+		cout << "Ваш пароль:  " << password << "\n";
+	}
+	else
+	{
+		cout << "Логин не может привышать длинну в 15 символов.";
+	}
+
 
 	return 0;
 }
