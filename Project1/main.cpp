3a7ddbe01e804bb583ea4925eac789e459f50180
skibidi <skibidielvin07@mail.ru>
2024-10-25 08:48:58 +0500

    123

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,47 +1,24 @@
 #include <iostream>
-#include <windows.h>
-#include <string>
-using namespace std;
+#include <Windows.h>
 
-bool logining(string login, string password, string login_pass, string password_pass)
+
+void Arbyz(int arr[], int size, int* arr1[])
 {
-	if (end(login) - begin(login) > 15 || end(password) - begin(password) > 15)
+	for (int i = 0; i < size; i++)
 	{
-		cout << "Ћогин и пароль не могут превышать лимит в 15 символов. ";
-		return false;
-	}
-	else if (login == login_pass && password == password_pass)
-	{
-		return true;
+		arr1[i] = &arr[i];
+		std::cout << arr1[i] << '\t';
 	}
 }
 
-int main(){
 
+
+int main()
+{
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-
-	string login = "arbyz";
-	string password = "olegovich";
-	string login_pass;
-	string password_pass;
-
-	cout << "¬ведите логин: ";
-	getline(cin, login_pass, '\n');
-	cout << "¬ведите пароль: ";
-	getline(cin, password_pass, '\n');
-
-	bool reg = logining(login, password, login_pass, password_pass);
-	if (reg == true)
-	{
-		cout << "¬ход прошел успешно \n";
-	}
-	else
-	{
-		cout << "ѕароль или логин введены неправильно.";
-	}
+	setlocale(LC_ALL, "1251");
 
 
-	return 0;
 }
